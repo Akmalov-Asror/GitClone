c5a1bf4dc17110a4127f0ad0716f9849ce600641
A3S4ROBOY <110540334+Akmalov-Asror@users.noreply.github.com>
2023-05-30 13:02:58 +0500

    bags

---
+++ MudBlazor/DTO's/GetGitHubRepositoriesDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MudBlazor.DTO_s;
+
+public class GetGitHubRepositoriesDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    public string? HtmlUrl { get; set; }
+    public string? Url { get; set; }
+}
--- MudBlazorGitRepo/DTO's/GetGitHubRepositoriesDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CloneRepoBlazor.DTO_s;
-
-public class GetGitHubRepositoriesDto
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; }
-}
